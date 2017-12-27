df66da66dbec6e2e20a0e2d187b1179e266db4e9
NikolaiGit <bubiseip@web.de>
2017-12-27 14:55:57 +0100

    initial commit from my tests

---
+++ src/TestRobot1/TestRobot1Main.java
@@ -0,0 +1,82 @@
+package TestRobot1;
+
+import robocode.*;
+
+public class TestRobot1Main extends Robot {
+
+	public void run() {
+
+		System.out.println(this.getClass().getName() + ".run");
+
+		/*while (true) {
+
+		}*/
+	}
+
+	
+	@Override
+	public void onBattleEnded(BattleEndedEvent e) {
+
+	}
+	
+	@Override
+	public void onBulletHit(BulletHitEvent e) {
+
+	}
+	
+	@Override
+	public void onBulletHitBullet(BulletHitBulletEvent e) {
+
+	}
+	
+	@Override
+	public void onBulletMissed(BulletMissedEvent e) {
+
+	}
+	
+	@Override
+	public void onDeath(DeathEvent e) {
+
+	}
+	
+	@Override
+	public void onHitByBullet(HitByBulletEvent e) {
+
+	}
+	@Override
+	public void onHitRobot(HitRobotEvent e) {
+
+	}
+	
+	@Override
+	public void onHitWall(HitWallEvent e) {
+
+	}
+	
+	@Override
+	public void onRobotDeath(RobotDeathEvent e) {
+
+	}
+	
+	@Override
+	public void onRoundEnded(RoundEndedEvent e) {
+
+	}
+	
+	@Override
+	public void onScannedRobot(ScannedRobotEvent e) {
+
+	}
+	
+	@Override
+	public void onStatus(StatusEvent e) {
+
+	}
+	
+	@Override
+	public void onWin(WinEvent e) {
+
+	}
+
+
+}
